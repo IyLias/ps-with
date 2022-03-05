ac662cb8220ac19378bb4e764e6923db69a8d131
IyLias <48081162+IyLias@users.noreply.github.com>
2022-03-05 15:50:25 +0900

    simple segment tree template implemented
    simple segment tree template implemented

--- Tree/segment_tree.cpp
+++ Tree/segment_tree.cpp
@@ -4,45 +4,47 @@
 #include <vector>
 #include <cmath>
 
-
 using namespace std;
 
-long long init(vector<long long> &arr,vector<long long> &tree,int node,int start,int end){
-    if(start==end)
-       return (tree[node] = arr[start]);
-    else
-       return (tree[node] = init(arr,tree,2*node,start,(start+end)/2) + init(arr,tree,2*node+1,(start+end)/2+1,end) );
+long long init(vector<long long> &arr,vector<long long> &tree,int node,int start,int end){ 
+    if(start==end) 
+        return (tree[node] = arr[start]); 
+    else 
+        return (tree[node] = init(arr,tree,2*node,start,(start+end)/2) + init(arr,tree,2*node+1,(start+end)/2+1,end) ); 
 }
 
-long long sum(vector<long long> &arr,vector<long long> &tree,int node,int start,int end,int left,int right){
-
-
-
+long long sum(vector<long long> &arr,vector<long long> &tree,int node,int start,int end,int left,int right){ 
+    if(left > end || right < start) 
+        return 0;
+    else if(left<=start && end<=right)
+        return tree[node];
+    else 
+        return sum(arr,tree,2*node,start,(start+end)/2,left,right) + sum(arr,tree,2*node+1,(start+end)/2+1,end,left,right);
 }
 
-long long update(vector<long long> &arr,vector<long long> &tree,int node,int index,int start,int end){
-
-
-
+void update(vector<long long> &arr,vector<long long> &tree,int node,int index,int start,int end,int k){
+    if(index == start && index == end){
+        tree[node] = k;
+        return tree[node];
+    }
+    else if(start<= index && index <= end){
+        return tree[node] = update(arr,tree,2*node,index,start,(int)((start+end)/2),k) + update(arr,tree,2*node+1,index,(int)((start+end)/2)+1,end,k);
+    }else
+        return tree[node];
 }
 
 int main(){
- 
- int n;
- scanf("%d",&n);
 
- vector<long long> arr(n);
+  int n; scanf("%d",&n);
 
- int h = (int)ceil(log2(n));
- int tree_size = (1 << (h+1));
- vector<long long> segment_tree(tree_size);
+  vector<long long> arr(n);
 
- for(int i=0;i<n;i++)
-   scanf("%lld",&arr[i]);
+  int h = (int)ceil(log2(n)); 
+  int tree_size = (1 << (h+1));
+  vector<long long> segment_tree(tree_size);
 
- 
- 
+  for(int i=0;i<n;i++) 
+    scanf("%lld",&arr[i]);
 
- 
- return 0;
+  return 0; 
 }
