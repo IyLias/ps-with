adf96cb5e1e4f6cb1f2dd71e1db7ed0458f3a8f2
IyLias <iliaskyrin9707@gmail.com>
2023-07-30 00:29:51 +0900

    solved Q1463-make 1

---
+++ DP/Q1463.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MAXNUM	1000000
+
+int times[MAXNUM+1];
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	times[1] = 0, times[2] = 1, times[3] = 1;
+
+	for (int i = 4; i <= N; i++)
+	{
+		times[i] = times[i - 1];
+		if (i % 2 == 0)
+			times[i] = min(times[i], times[i / 2]);
+		if(i % 3 == 0)
+			times[i] = min(times[i], times[i / 3]);
+
+		times[i] += 1;
+	}
+
+	printf("%d\n", times[N]);
+
+	return 0;
+}
+
