d3ea68b345ecccddb3b096ca940701279d6ac575
IyLias <iliaskyrin9707@gmail.com>
2023-08-02 19:10:54 +0900

    solved stairs number, increasing number

---
+++ DP/Q10844.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int dp[101][10];
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	dp[1][0] = 0;
+	for (int i = 1; i < 10; i++)
+		dp[1][i] = 1;
+
+	for (int i = 2; i <= N; i++) {
+
+		for (int j = 0; j < 10; j++) {
+
+			if (j == 0) dp[i][j] = dp[i - 1][j + 1];
+			else if (j == 9) dp[i][j] = dp[i - 1][j - 1];
+			else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1])%1000000000;
+		}
+
+	}
+
+	int result = 0;
+	for (int i = 0; i < 10; i++)
+		result = (result + dp[N][i]) % 1000000000;
+
+	printf("%d\n", result);
+
+	return 0;
+}
+
---
+++ DP/Q11057.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int dp[1001][10];
+
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < 10; i++)
+		dp[1][i] = 1;
+
+
+	for (int i = 2; i <= N; i++) {
+
+		dp[i][0] = 1;
+		for (int j = 1; j < 10; j++) {
+			dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
+		}
+
+	}
+
+
+	int result = 0;
+	for (int i = 0; i < 10; i++)
+		result += dp[N][i];
+	printf("%d\n", result);
+
+	return 0;
+}
+
