35aaacce50357bc3b18873a4ce603143eff8adca
IyLias <iliaskyrin9707@gmail.com>
2023-09-05 23:00:41 +0900

    solved some problems using binary search

---
+++ binary-search/Q1920.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <set>
+
+#define MAXNUM  100001
+
+int A[MAXNUM];
+
+using namespace std;
+
+bool search(int first,int last, int target) {
+
+	int start = first;
+	int end = last;
+
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (A[mid] < target)
+			start = mid + 1;
+		else if (A[mid] > target)
+			end = mid - 1;
+		else
+			return true;
+	}
+
+	return false;
+}
+
+
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+	sort(A, A + N);
+
+	int M;
+	scanf("%d", &M);
+
+	for (int i = 0; i < M; i++) {
+
+		int num;
+		scanf("%d", &num);
+
+		bool result = search(0,N-1,num);
+		printf("%d\n", result == true ? 1 : 0);
+	}
+
+
+	return 0;
+}
+
---
+++ binary-search/Q2295.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <set>
+
+#define MAXNUM  1001
+
+using namespace std;
+
+int U[MAXNUM];
+vector<int> summ;
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &U[i]);
+	}
+
+	sort(U, U + N);
+
+	for (int i = 0; i < N; i++)
+		for (int j = i; j < N; j++)
+			summ.push_back(U[i] + U[j]);
+	sort(summ.begin(), summ.end());
+
+	for (int j = N-1; j >= 0; j--)
+		for (int k = j; k >= 0; k--)
+		{
+			int num = U[j] - U[k];
+			if (binary_search(summ.begin(), summ.end(), num)) {
+				printf("%d\n", U[j]);
+				return 0;
+			}
+
+		}
+
+
+	return 0;
+}
