b4a1bf2707ffc9a06c792644bd6c3fed7251bc35
IyLias <iliaskyrin9707@gmail.com>
2023-09-23 15:15:08 +0900

    solved some floyd-washall algorithm problems: n-phase statement problem

---
+++ graph/floyd/Q15723.cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <set>
+
+#define MAXSIZE	27
+
+bool alphabet_map[MAXSIZE][MAXSIZE];
+// a b c d 
+
+using namespace std;
+
+void search() {
+
+	for (int k = 0; k < MAXSIZE; k++)
+		for (int i = 0; i < MAXSIZE; i++)
+			for (int j = 0; j < MAXSIZE; j++)
+			{
+				if (alphabet_map[i][k] && alphabet_map[k][j])
+					alphabet_map[i][j] = true;
+			}
+	
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) {
+		string statement;
+		getline(cin, statement);
+
+		char p = statement[0], q = statement[5];
+		int start = statement[0] - 'a';
+		int end = statement[5] - 'a';
+		alphabet_map[start][end] = true;
+	}
+
+	search();
+
+	int m;
+	scanf("%d", &m);
+	cin.ignore();
+
+	for (int i = 0; i < m; i++) {
+		string statement;
+		getline(cin, statement);
+
+		char p = statement[0], q = statement[5];
+		int start = statement[0] - 'a';
+		int end = statement[5] - 'a';
+
+		printf("%c\n", alphabet_map[start][end] ? 'T' : 'F');
+	}
+
+}
+
---
+++ graph/floyd/Q2224.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <set>
+
+#define INF	98765432
+#define MAXSIZE	54
+
+int alphabet_map[MAXSIZE][MAXSIZE];
+// A B C D ... a b c d 
+
+using namespace std;
+
+int search() {
+
+	for(int k=0;k<MAXSIZE;k++)
+		for(int i=0;i<MAXSIZE;i++)
+			for (int j = 0; j < MAXSIZE; j++)
+			{
+				if (alphabet_map[i][k] != INF && alphabet_map[k][j] != INF)
+					alphabet_map[i][j] = min(alphabet_map[i][j], alphabet_map[i][k] + alphabet_map[k][j]);
+			}
+
+	int result = 0;
+	for (int i = 0; i < MAXSIZE; i++)
+		for (int j = 0; j < MAXSIZE; j++)
+			if (i != j && alphabet_map[i][j] != 0 && alphabet_map[i][j] != INF)
+				result++;
+
+	return result;
+}
+
+
+int main()
+{
+	int X;
+	scanf("%d", &X);
+	cin.ignore();
+
+	for (int i = 0; i < MAXSIZE; i++)
+		for (int j = 0; j < MAXSIZE; j++)
+			alphabet_map[i][j] = INF;
+
+	for (int i = 0; i < X; i++) {
+		string statement;
+		getline(cin,statement);
+
+		char p = statement[0], q = statement[5];
+		int start = statement[0] >= 'a' ? statement[0] - 'a' + 26 : statement[0] - 'A';
+		int end = statement[5] >= 'a' ? statement[5] - 'a' + 26 : statement[5] - 'A';
+		alphabet_map[start][end] = 1;
+
+	}
+
+	int result = search();
+
+	printf("%d\n", result);
+	for(int i=0;i<MAXSIZE;i++)
+		for(int j=0;j<MAXSIZE;j++)
+			if (i != j && alphabet_map[i][j]!=0 && alphabet_map[i][j] != INF) {
+				char p = i >= 26 ? i - 26 + 'a' : i + 'A';
+				char q = j >= 26 ? j - 26 + 'a' : j + 'A';
+				printf("%c => %c\n", p, q);
+			}
+
+}
+
