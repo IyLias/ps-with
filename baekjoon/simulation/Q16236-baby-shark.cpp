acd6c9cf856391b7b607c985059945e890553b1a
IyLias <iliaskyrin9707@gmail.com>
2024-07-03 00:03:24 +0900

    [DEV] started solving Q16236 baby shark problem

--- baekjoon/simulation/Q16236-baby-shark.cpp
+++ baekjoon/simulation/Q16236-baby-shark.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 
+class BabyShark{
+
+ public:
+   BabyShark(){
+      this->size = 2;
+   }
+
+   int get_size(){
+	return size;
+   }
+ 
+   int behave(){
+	
+
+   }
+
+
+ private:
+	
+   int size;	
+
+
+
+
+};
 
 int main(){
 
